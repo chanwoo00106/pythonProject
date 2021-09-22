83ad7449a97b0647f6d103da12bbde756f897432
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-22 19:39:37 +0900

    Update commit.py
    최적화

--- git_ranking_discord/commit.py
+++ git_ranking_discord/commit.py
@@ -16,9 +16,9 @@ def commitNum(name):
     num = 0
 
     for i in soup.select(".filter-list.small li"):
-        html = get(f'https://github.com/{name}?tab=overview&from={int(i.get_text())}-01-01&to={int(i.get_text())}-12-31')
+        html = get(f'https://github.com/users/{name}/contributions?tab=overview&from={int(i.get_text())}-01-01&to={int(i.get_text())}-12-31')
         soup = BeautifulSoup(html.text, 'html.parser')
-        commit = soup.select_one(".js-yearly-contributions > .position-relative > .f4").get_text()
+        commit = soup.select_one(".f4.text-normal.mb-2").get_text()
         list = str(commit).split(' ')
 
         for i in range(0, len(list)):
