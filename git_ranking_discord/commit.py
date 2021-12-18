ce8b0bb2df9ff17183b22d4d611ffd726e5f13a9
변찬우 <chan00106@naver.com>
2021-12-18 20:29:15 +0900

    delete ranking

--- git_ranking_discord/commit.py
+++ git_ranking_discord/commit.py
@@ -1,19 +1,11 @@
 from requests import get
 from bs4 import BeautifulSoup
 import json
-from dotenv import load_dotenv
 import os
-load_dotenv()
-FILTER = os.environ.get('FILTER')
+
 
 
 def commitNum(name):
-    data = json.load(open('./ranking.json', 'r', encoding='utf-8'))
-
-    for i in range(1, 19):
-        if data.get(f'{i}')[0]['name'] == name:
-            name = data[f'{i}'][0]['id']
-
     html = get(f'https://github.com/{name}')
 
     soup = BeautifulSoup(html.text, 'html.parser')
@@ -38,62 +30,4 @@ def commitNum(name):
             except ValueError:
                 pass
 
-    # 거를 사람 정하기
-    # if name == FILTER:
-    #     num -= 1000
-
-    for i in range(1, 19):
-        user = data.get(str(i))[0]
-        if user.get("id") == name:
-
-            data.get(f"{i}")[0]["commit"] = num
-
-            with open('./ranking.json', 'w', encoding='utf-8') as outfile:
-                json.dump(data, outfile, indent=4, ensure_ascii=False)
     return [num, f'https://github.com/{name}']
-
-
-def ranking():
-    data = json.load(open('./ranking.json', 'r', encoding='utf-8'))
-    rankingDic = {}
-    for i in range(0, 5):
-        topCommitUser = [0, ""]
-        tempJ = 0
-
-        for j in range(1, 19):
-            try:
-                user = data.get(f"{j}")[0]
-            except TypeError:
-                continue
-
-            if topCommitUser[0] <= user.get("commit"):
-                tempJ = j
-                topCommitUser[0] = user.get("commit")
-                topCommitUser[1] = user.get("name")
-        data.pop(f"{tempJ}")
-        rankingDic[topCommitUser[1]] = topCommitUser[0]
-
-    return rankingDic
-
-
-def rankingNum(top):
-    data = json.load(open('./ranking.json', 'r', encoding='utf-8'))
-    rankingDic = {}
-    for i in range(0, top):
-        topCommitUser = [0, ""]
-        tempJ = 0
-
-        for j in range(1, 19):
-            try:
-                user = data.get(f"{j}")[0]
-            except TypeError:
-                continue
-
-            if topCommitUser[0] <= user.get("commit"):
-                tempJ = j
-                topCommitUser[0] = user.get("commit")
-                topCommitUser[1] = user.get("name")
-        data.pop(f"{tempJ}")
-        rankingDic[topCommitUser[1]] = topCommitUser[0]
-
-    return rankingDic
--- git_ranking_discord/main.py
+++ git_ranking_discord/main.py
@@ -6,39 +6,8 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 TOKEN = os.environ.get('TOKEN')
-ROOMID = os.environ.get('ROOMID')
 
 client = discord.Client()
-
-
-complateDay = False
-
-
-async def alarm():
-    global complateDay
-    user = client.get_channel(ROOMID)
-
-    while True:
-        while not complateDay:
-            now = datetime.datetime.now()
-            if now.hour >= 9 and (now.minute == 0 or now.minute == 30):
-                await user.send('커밋해!!!')
-                await asyncio.sleep(60)
-            elif now.minute == 0:
-                await user.send('커밋해라!! 인간')
-                await asyncio.sleep(60)
-            else:
-                await asyncio.sleep(1)
-
-        while complateDay:
-            now = datetime.datetime.now()
-            # 한국 시간과 서버를 돌리는 컴퓨터의 시간차가 있음
-            # 여기서 23시는 한국에서 9시
-            if now.hour == 23:
-                complateDay = False
-                await asyncio.sleep(1)
-            else:
-                await asyncio.sleep(1)
 
 
 @client.event
@@ -55,47 +24,6 @@ async def on_message(message):
     global complateDay
     if message.author.bot:
         return None
-
-    if message.content.startswith('!커밋 완료') and message.author.id == ROOMID and not complateDay:
-        complateDay = True
-        channel = message.channel
-        await channel.send('확인!')
-
-    if message.content == 'git ranking':
-        print('git ranking 준비중')
-        channel = message.channel
-
-        rankingDic = commit.ranking()
-        j = 1
-        say = ""
-        for i in rankingDic:
-            say = say + f"{j}. {i} : {rankingDic[i]} commit\n"
-            j = j + 1
-        embed = discord.Embed(title="1-2 Commit Ranking",
-                              description=say, color=0xdff9fb)
-        await channel.send(embed=embed)
-
-    if message.content.startswith('git ranking '):
-        print('git ranking 준비중')
-        channel = message.channel
-        message.content = message.content.replace('git ranking ', '')
-        try:
-            if int(message.content) > 0 and int(message.content) <= 18:
-                rankingDic = commit.rankingNum(int(message.content))
-            else:
-                await channel.send('0 ~ 18위 까지')
-                return None
-        except:
-            await channel.send('`git ranking {숫자}` 형식으로 작성')
-            return None
-        j = 1
-        say = ""
-        for i in rankingDic:
-            say = say + f"{j}. {i} : {rankingDic[i]} commit\n"
-            j = j + 1
-        embed = discord.Embed(
-            title=f"1-2 Commit Ranking top {message.content}", description=say, color=0xdff9fb)
-        await channel.send(embed=embed)
 
     if message.content.startswith('!git ') or message.content.startswith('!햣 '):
         channel = message.channel
